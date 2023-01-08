129161b68d0ca6d3d50d6ccedc591886cf62e8e3
robin828 <61390860+robin828@users.noreply.github.com>
2023-01-08 19:43:23 +0530

    done

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -10,17 +10,23 @@ function Listing() {
   const listInnerRef = useRef();
   const [currPage, setCurrPage] = useState(1);
   const [loading, setLoading] = useState(true);
+  const [uid, setUid] = useState("");
 
   const dispatch = useDispatch();
 
-
+    
   useEffect(() => {
-
-    axios.post('https://api.mentro.tech/api/v3/users/getMentorList',{ limit: 20, docId: "" }).then((res)=>{
+    console.log("fhjghfjkdhg", uid)
+    axios.post('https://api.mentro.tech/api/v3/users/getMentorList', { limit: 20, docId: uid }).then((res)=>{
       dispatch({type: actions.ADD_MENTOR, payload: {mentorList: res.data.mentorList}});
+    //   console.log("uid", res.data.mentorList)
+    setUid(res.data.mentorList[19].mentorData.uid)
+    console.log(res.data.mentorList[19].mentorData.uid)
+    console.log(res.data.mentorList, "P{P}{P")
+    //   setUid(res.data.mentorList[res.data.mentorList.length-1].uid)
       setLoading(false);
     })  
-    
+    console.log(uid, "PPPOOPOP")
   }, [currPage]);
   const userList = useSelector((state)=>state);
 
--- src/components/ListingPage.js
+++ src/components/ListingPage.js
@@ -21,16 +21,79 @@ function ListingPage({ onScroll, listInnerRef, userList }) {
           userList.map((item, index) => {
             return (
               <>
-                <Card style={{ width: "19rem" }}>
+                <Card style={{ width: "19rem", margin: '2px' }}>
                   <Card.Img
                     variant="top"
                     src={item.mentorData.profileUrl}
                     style={{ width: "10rem", height: "10rem" }}
                   />
                   <Card.Body>
-                    <Card.Title>{item.mentorData.name}</Card.Title>
-                    <Card.Text>
+                    <Card.Title
+                      style={{
+                        color: "#424259",
+                        fontFamily: "Poppins,sans-serif",
+                        fontSize: "1.15em",
+                        fontWeight: 500,
+                        margin: 0,
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        whiteSpace: "nowrap",
+                      }}
+                    >
+                      {item.mentorData.name}
+                    </Card.Title>
+                    <Card.Text
+                      style={{
+                        color: "#8e939f",
+                        fontFamily: "Poppins,sans-serif",
+                        fontSize: "1em",
+                        fontWeight: 400,
+                        margin: "0.3em 0 0",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        whiteSpace: "nowrap",
+                        width: "100%",
+                      }}
+                    >
                       {item.mentorData.role} at {item.mentorData.organisation}
+                    </Card.Text>
+                    <Card.Text
+                    >
+                      {item.mentorData.interests.slice(0, 2).map((i, ind) => {return <>
+                      <span style={{
+                        background: "rgba(76,175,80,.08)",
+                        borderRadius: "4px",
+                        color: "#4caf50",
+                        display: "inline-block",
+                        fontFamily: "Poppins,sans-serif",
+                        fontSize: ".8em",
+                        fontWeight: 400,
+                        margin: "0.4em 0.5em 0 0",
+                        maxWidth: "150px",
+                        overflow: "hidden",
+                        padding: "0.3em 1em",
+                        textAlign: "center",
+                        textOverflow: "ellipsis",
+                        whiteSpace: "nowrap",
+                      }}>{i + "  " }</span>
+                        
+                      </>})}
+                      {/* <span style={{
+                        background: "rgba(76,175,80,.08)",
+                        borderRadius: "4px",
+                        color: "#4caf50",
+                        display: "inline-block",
+                        fontFamily: "Poppins,sans-serif",
+                        fontSize: ".8em",
+                        fontWeight: 400,
+                        margin: "0.4em 0.5em 0 0",
+                        maxWidth: "150px",
+                        overflow: "hidden",
+                        padding: "0.3em 1em",
+                        textAlign: "center",
+                        textOverflow: "ellipsis",
+                        whiteSpace: "nowrap",
+                      }}>{item.mentorData.interests[0]+ "  "}</span> */}
                     </Card.Text>
                     <Button
                       style={{
